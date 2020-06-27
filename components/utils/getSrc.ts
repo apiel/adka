d30f94fc16121ff8cc75ea7e3712b762d691156e
Alexandre Piel <alexandre.piel@gmail.com>
2020-06-27 22:52:56 +0200

    ci getSrc

--- components/utils/getSrc.ts
+++ components/utils/getSrc.ts
@@ -2,7 +2,7 @@ import { dirname, join } from 'https://deno.land/std/path/mod.ts';
 import { caller } from '../../deps.ts';
 
 export function getSrc(src: string) {
-    console.log('call getSrc', src);
+    console.log(`call getSrc "${src}"`);
     if (src.startsWith('.')) {
         // !! caller must stay in this function
         const parentFile = caller.default(caller.up + 1);
